e4e6799794768201be8860f8eee63c2d9f386f29
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-03-28 06:24:17 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+function App() {
+  return (
+    <>
+      <h1>You got this 💪</h1>
+      <p>Stuck? Not sure how to proceed?</p>
+      <p>Don't worry - we've all been there. Let's build it together!</p>
+    </>
+  );
+}
 
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default App;
